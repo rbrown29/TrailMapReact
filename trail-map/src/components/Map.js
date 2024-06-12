77b2685a430bccb48a06ab78252fd20a5786c840
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2024-06-12 10:53:07 -0700

    Update Map.js

--- trail-map/src/components/Map.js
+++ trail-map/src/components/Map.js
@@ -14,7 +14,7 @@ const MapComponent = ({ trail }) => {
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/satellite-v9',
       center: [trail.data[0].lon, trail.data[0].lat],
-      zoom: 14,
+      zoom: isMobile ? 12 : 14,
       pitch: isMobile ? 20 : 40,  
       bearing: isMobile ? 90 : 150, 
       antialias: true,
