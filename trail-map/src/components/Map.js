e2ac456ea932ead8791e46a2cf7f1c17778e7d2c
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2024-11-23 11:06:00 -0800

    added new trail

--- trail-map/src/components/Map.js
+++ trail-map/src/components/Map.js
@@ -116,10 +116,10 @@ const MapComponent = ({ trail }) => {
     // Smith Rock 0, Adams Glacier 0, Angel's Rest 0, eagle creek 0, Multnomah-Wahkeena Loop 0, cape perpetua -120, battleAxeLoop 0, belknapCrater 0, Ice Lake 0, cached lake 0, Garfield Peak 0, cape falcon 0, cape kiwanda 0, cascadeHead 0, mirror lake 0, opal creek 0, tamolitch 0
     
     map.on('load', () => {
-      const animationDuration = 80000;
-      const cameraAltitude = 2000;
-      const pitch = 30;
-      const startBearing = 0;
+      const animationDuration = 50000;
+      const cameraAltitude = 2500;
+      const pitch = 20;
+      const startBearing = 90;
 
       const routeDistance = length(lineString(targetRoute));
 
